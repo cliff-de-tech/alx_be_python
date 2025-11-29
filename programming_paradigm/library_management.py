dfe62254e3867d05356f42aec561d9d3ee6b490d
cliff-de-tech <cliffdesignz@gmail.com>
2025-11-29 13:24:45 +0000

    feat: add library management system

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,57 @@
+class Book:
+    """A class to represent a book in a library."""
+
+    def __init__(self, title, author):
+        """Initialize a new Book instance."""
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    """A class to manage a collection of books in a library."""
+
+    def __init__(self):
+        """Initialize a new Library instance."""
+        self._books = []
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by its title."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+        print(f"Sorry, '{title}' is not available for check out.")
+
+    def return_book(self, title):
+        """Return a book by its title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+        print(f"Sorry, '{title}' was not checked out from this library.")
+
+    def list_available_books(self):
+        """List all available books in the library."""
+        available_books = [book for book in self._books if book.is_available()]
+        if not available_books:
+            print("No books are currently available.")
+        else:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
